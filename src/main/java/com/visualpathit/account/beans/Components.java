f079995748f2bede039b47fa8db4f10263a015b5
dheeraj kjandpal <dheeraj.kandpal89@gmail.com>
2026-02-28 23:16:58 +0530

    Add a blank line for improved readability in Components.java

--- src/main/java/com/visualpathit/account/beans/Components.java
+++ src/main/java/com/visualpathit/account/beans/Components.java
@@ -3,6 +3,7 @@ package com.visualpathit.account.beans;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class Components {
 	
